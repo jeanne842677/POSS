ddf7db056401e2ffe973d79ad82ac9a06058e140
kyungmin <alssgo70051@naver.com>
2021-09-16 14:44:07 +0900

    [경민]푸쉬푸쉬

--- src/com/kh/poss/common/db/JDBCTemplate.java
+++ src/com/kh/poss/common/db/JDBCTemplate.java
@@ -1,10 +1,12 @@
 package com.kh.poss.common.db;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+import oracle.ucp.jdbc.PoolDataSource;
+import oracle.ucp.jdbc.PoolDataSourceFactory;
 
 //dao안에 중복되는 메서드를 줄여줄 템플릿
 
@@ -14,42 +16,82 @@ import java.sql.Statement;
 //2. static 필드변수를 만듦   :싱글톤 패턴
 //3. getInstance() 메서드 만듦
 
-
 public class JDBCTemplate {
 
 	private static JDBCTemplate instance;
+
+	 PoolDataSource pds;
 	// 드라이버 열기
 	private JDBCTemplate() {
-		
+
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
 
-		} catch (ClassNotFoundException e) {
+			String url = "jdbc:oracle:thin:@possdb_high?TNS_ADMIN=C:/CODE/before/e_servlet/Wallet_possDB";
+			String userId = "ADMIN";
+			String password = "Rngusgowh123";
+			
+			final String CONN_FACTORY_CLASS_NAME="oracle.jdbc.pool.OracleDataSource";
+			
+			 pds = PoolDataSourceFactory.getPoolDataSource();
+
+			    // Set the connection factory first before all other properties
+			    pds.setConnectionFactoryClassName(CONN_FACTORY_CLASS_NAME);
+			    pds.setURL(url);
+			    pds.setUser(userId);
+			    pds.setPassword(password);
+			    pds.setConnectionPoolName("JDBC_UCP_POOL");
+
+			    // Default is 0. Set the initial number of connections to be created
+			    // when UCP is started.
+			    pds.setInitialPoolSize(5);
+
+			    // Default is 0. Set the minimum number of connections
+			    // that is maintained by UCP at runtime.
+			    pds.setMinPoolSize(5);
+
+			    // Default is Integer.MAX_VALUE (2147483647). Set the maximum number of
+			    // connections allowed on the connection pool.
+			    pds.setMaxPoolSize(20);
+
+			    // Default is 30secs. Set the frequency in seconds to enforce the timeout
+			    // properties. Applies to inactiveConnectionTimeout(int secs),
+			    // AbandonedConnectionTimeout(secs)& TimeToLiveConnectionTimeout(int secs).
+			    // Range of valid values is 0 to Integer.MAX_VALUE. .
+			    pds.setTimeoutCheckInterval(5);
+
+			    // Default is 0. Set the maximum time, in seconds, that a
+			    // connection remains available in the connection pool.
+			    pds.setInactiveConnectionTimeout(10);
+			
+
+		} catch (ClassNotFoundException | SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 	}
-	
+
 	public static JDBCTemplate getInstance() {
-		if(instance ==null) {
-		instance = new JDBCTemplate();
+		if (instance == null) {
+			instance = new JDBCTemplate();
 		}
 		return instance;
-		
+
 	}
 
 	// 데이터 베이스와 연결 수립: Connection 객체 만들기
 	public Connection getConnection() {
 		Connection conn = null;
-		String url = "jdbc:oracle:thin:@localhost:1521:xe";
-		String userId = "bm";
-		String password = "1234";
-		try {
-			conn = DriverManager.getConnection(url, userId, password);
 
+
+		try {
+
+			conn = pds.getConnection();
 			// 트랜젝션 관리를 개발하기 하기 위해 Auto commit 끄기
 			conn.setAutoCommit(false); // 세션 완료시에는 commit이 됨.
+			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -83,7 +125,6 @@ public class JDBCTemplate {
 
 	}
 
-	
 	// 4. close()
 	public void close(Connection conn) {
 		try {
@@ -122,47 +163,36 @@ public class JDBCTemplate {
 
 	}
 
-	public void close(ResultSet rset,Statement stmt,Connection conn) {
-		
-		
+	public void close(ResultSet rset, Statement stmt, Connection conn) {
+
 		close(rset);
 		close(stmt);
 		close(conn);
 		/*
-		try {
-			
-			
-			if (rset != null && !rset.isClosed()) {
-				rset.close();
-			}
-			if (stmt != null && !stmt.isClosed()) {
-				stmt.close();
-			}
-			
-			if (conn != null && !conn.isClosed()) {
-				conn.close();
-			}
-			
-			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		*/
+		 * try {
+		 * 
+		 * 
+		 * if (rset != null && !rset.isClosed()) { rset.close(); } if (stmt != null &&
+		 * !stmt.isClosed()) { stmt.close(); }
+		 * 
+		 * if (conn != null && !conn.isClosed()) { conn.close(); }
+		 * 
+		 * 
+		 * } catch (SQLException e) { // TODO Auto-generated catch block
+		 * e.printStackTrace(); }
+		 */
 
 	}
-	
-	public void close(Statement stmt , Connection conn) {
+
+	public void close(Statement stmt, Connection conn) {
 		close(stmt);
 		close(conn);
 	}
-	
-	
-	public void close(ResultSet rset , Statement stmt) {
+
+	public void close(ResultSet rset, Statement stmt) {
 		close(rset);
 		close(stmt);
-		
+
 	}
-	
-	
+
 }
